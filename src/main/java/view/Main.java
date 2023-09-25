f6e835b2eced3bad3998f2a1daa2320dce8404c0
DAO <DAO@LAPTOP-SDT31U9Q>
2023-09-25 08:12:43 +0700

    First Commit

---
+++ src/main/java/view/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.Fruit_Shop_System;
+
+/**
+ *
+ * @author DAO
+ */
+public class Main {
+    public static void main(String[] args) {
+            Fruit_Shop_System fss = new Fruit_Shop_System();
+            fss.run2();
+    }
+}
